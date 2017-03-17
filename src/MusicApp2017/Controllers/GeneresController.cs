2e22c93fae19bbd77e3829a1c66ccf6e82dfb180
ryanceresani <ryanceresani@gmail.com>
2017-03-17 11:51:51 -0400

    Adding Genre Section

---
+++ src/MusicApp2017/Controllers/GeneresController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MusicApp2017.Models;
+
+namespace MusicApp2017.Controllers
+{
+    public class GenresController : Controller
+    {
+        private readonly MusicDbContext _context;
+
+        public GenresController(MusicDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Genres
+        public async Task<IActionResult> Index()
+        {
+            var musicDbContext = _context.Genres;
+            return View(await musicDbContext.ToListAsync());
+        }
+
+        // GET: Genres/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var genreContext = _context.Genres;
+                //.Include(g => g.Name); 
+            var genre = await genreContext
+                .SingleOrDefaultAsync(m => m.GenreID == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // GET: Albums/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Albums/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AlbumID,Title,ArtistID,GenreID,Likes")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(album);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["ArtistID"] = new SelectList(_context.Artists, "ArtistID", "Name", album.ArtistID);
+            ViewData["GenreID"] = new SelectList(_context.Genres, "GenreID", "Name", album.GenreID);
+            return View(album);
+        }
+
+        // GET: Albums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums.SingleOrDefaultAsync(m => m.AlbumID == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            ViewData["ArtistID"] = new SelectList(_context.Artists, "ArtistID", "Name", album.ArtistID);
+            ViewData["GenreID"] = new SelectList(_context.Genres, "GenreID", "Name", album.GenreID);
+            return View(album);
+        }
+
+        // POST: Albums/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("GenreID,Name,Likes")] Genre genre)
+        {
+            if (id != genre.GenreID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(genre);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GenreExists(genre.GenreID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+        private bool GenreExists(int id)
+        {
+            return _context.Genres.Any(e => e.GenreID == id);
+        }
+    }
+}
